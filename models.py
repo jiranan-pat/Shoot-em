5fa2c2eca3c71ef3753beb31c5890c59592a91ae
jiranan-pat <jiranan.pat@ku.th>
2019-05-03 00:26:01 +0700

    Update background and add aiming

--- models.py
+++ models.py
@@ -1,18 +1,63 @@
-class Player:
+import random
+
+TARGET_SPEED = 5
+BULLET_SPEED = 5
+
+class Gun:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = 70
+ 
+    def update(self, delta):
+        pass
+
+class Target:
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
- 
+
     def update(self, delta):
         pass
+
+class Bullet:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = 70
+
+class Fire:
+    pass
+
+class Aim:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+
+
  
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
- 
-        self.player = Player(self, width // 2, height // 2)
- 
+        
+        self.gun = Gun(self, width // 2, height // 2)
+        self.bullet = Bullet(self, width // 2, height // 2)
+        self.aim = Aim(self, width // 2, height // 2)
+
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.aim.x = x
+        self.aim.y = y
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        pass
+
     def update(self, delta):
-        self.player.update(delta)
+        self.gun.update(delta)
+
+    
+
+
--- shootem.py
+++ shootem.py
@@ -1,7 +1,7 @@
 import arcade
 from models import World
  
-SCREEN_WIDTH = 1024
+SCREEN_WIDTH = 960
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Shoot 'Em"
 
@@ -20,24 +20,63 @@ class ModelSprite(arcade.Sprite):
         self.sync_with_model()
         super().draw()
  
+class TargetSprite(arcade.Sprite):
+    def __init__(self, model):
+        self.model = model
+        self.target_sprite = arcade.Sprite(
+            'images/target_red2_outline.png')
+
+    def draw(self):
+        self.target_sprite.set_position(self.model.x, self.model.y)
+        self.target_sprite.draw()
+
+class BulletSprite(arcade.Sprite):
+    def __init__(self, model):
+        self.model = model
+        self.bullet_sprite = arcade.Sprite(
+            'images/bulletYellow_outline.png')
+
+    def draw(self):
+        self.bullet_sprite.set_position(self.model.x, self.model.y)
+        self.bullet_sprite.draw()
  
 class ShootEmWindow(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
- 
-        arcade.set_background_color(arcade.color.AMAZON)
- 
+        
+        self.set_mouse_visible(False)
+        self.background = arcade.load_texture(
+            "./images/background.png")
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
- 
-        self.dot_sprite = ModelSprite('images/shotgun.png', model=self.world.player)
- 
+
+        self.gun_sprite = ModelSprite('images/shotgunv2.png', model=self.world.gun)
+        # self.bullet_sprite = ModelSprite('images/bulletYellow_outline.png', model=self.world.bullet)
+        self.aim_sprite = ModelSprite('images/rsz_aiming.png', model=self.world.aim)
+
+
+    def draw_background(self):
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+    
     def update(self, delta):
         self.world.update(delta)
  
     def on_draw(self):
         arcade.start_render()
- 
-        self.dot_sprite.draw()
+
+        self.draw_background()
+        self.gun_sprite.draw()
+        # self.bullet_sprite.draw()
+        self.aim_sprite.draw()
+        output = f"Score: 0"
+        arcade.draw_text(output, 15, 570, arcade.color.BLACK, 16)
+
+    
+    def on_mouse_press(self, x, y, button, modifiers):
+        self.world.on_mouse_press(x, y, button, modifiers)
+    
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.world.on_mouse_motion(x, y, dx, dy)
  
  
 def main():
